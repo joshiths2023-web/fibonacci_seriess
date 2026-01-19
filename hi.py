44c02650e2952236d3138df8fdede0452ca7fe7a
joshiths2023-web <joshith.s2023@vitstudent.ac.in>
2026-01-19 10:51:29 +0530

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    """
+    Generate Fibonacci series up to n terms.
+    """
+    series = []
+    a, b = 0, 1
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+
+# Example usage
+num_terms = 10
+print(f"Fibonacci series up to {num_terms} terms:")
+print(fibonacci(num_terms))
